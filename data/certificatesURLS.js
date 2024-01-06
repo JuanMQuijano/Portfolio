2f6c6d4c2e1b6a3c4a550685e170d7508d32bcc9
Juan Q <jmquijano@unimayor.edu.co>
2024-01-06 18:05:23 -0500

    Corrección en rutas

--- data/certificatesURLS.js
+++ data/certificatesURLS.js
@@ -1,3 +1,5 @@
+const PATH = "./images/";
+
 export const certificatesURLS = [
   "https://udemy-certificate.s3.amazonaws.com/image/UC-f7637c0b-9818-4f85-b129-5fa65c28b93e.jpg",
   "https://udemy-certificate.s3.amazonaws.com/image/UC-3c9a6b25-ec18-4edf-b69f-a4356e731fa8.jpg",
@@ -5,6 +7,6 @@ export const certificatesURLS = [
   "https://udemy-certificate.s3.amazonaws.com/image/UC-f81ddbd9-dfc9-4a60-820e-54a4410b5a89.jpg",
   "https://udemy-certificate.s3.amazonaws.com/image/UC-dabb26ef-3811-4ef4-b384-1f694086eb87.jpg",
   "https://udemy-certificate.s3.amazonaws.com/image/UC-83a027fa-07f1-4924-82ab-3780c13ab6ab.jpg",
-  "./src/assets/c1.webp",
-  "./src/assets/c2.webp",
+  `${PATH}c1.webp`,
+  `${PATH}c2.webp`,
 ];
